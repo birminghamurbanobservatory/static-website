8041716d8960320dfb9e996d4bdd56fc05703664
SiBell <SiBellUK@gmail.com>
2019-10-07 21:40:31 +0100

    404 page improved

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container text-center mt-3">
+      <h1 className="text-primary">NOT FOUND</h1>
+      <p className="my-2">You just hit a route that doesn&#39;t exist.</p>
+      <p className="my-2">Return to the <Link to="/" className="text-link">Home Page</Link>.</p>
+    </div>
   </Layout>
 )
 
