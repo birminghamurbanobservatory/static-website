aa444862566e09cb9b69de84f94c872aa1c4bdec
SiBell <SiBellUK@gmail.com>
2019-10-08 22:34:32 +0100

    blog links tidied

--- src/components/blog-post-link.js
+++ src/components/blog-post-link.js
@@ -2,8 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 import Img from 'gatsby-image'
 const BlogPostLink = ({ post }) => (
-  <div className="w-full md:w-1/2 p-2">
-    <Link to={post.frontmatter.path} className="border border-gray-400 rounded p-4 h-full block hover:bg-teal-500">
+  <div className="w-full md:w-1/2 p-4">
+    <Link to={post.frontmatter.path} className="bg-gray-300 rounded p-4 h-full block hover:bg-teal-500">
       <h2 className="text-primary font-bold">{post.frontmatter.title}</h2>
       <p className="text-sm italic mb-3">({post.frontmatter.date})</p>
       { post.frontmatter.featuredImage &&
